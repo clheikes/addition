44a4eacd8661daca007bdabb7bff087af969c770
Clynt Heikes <24684947+clheikes@users.noreply.github.com>
2024-02-07 20:36:37 -0600

    add comments

--- lib.go
+++ lib.go
@@ -1,6 +1,11 @@
+// Package addition provides a function for adding two integers
 package addition
 
+// Add takes two integers and adds them together.
+//
+// It has two parameters: both parameters are integers, Add returns the sum of the
+// two integer parameters
+// [Addition](https://www.mathsisfun.com/numbers/addition.html)
 func Add(a, b int) int {
 	return a + b
 }
-
